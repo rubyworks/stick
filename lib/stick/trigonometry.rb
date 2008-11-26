bb3b4b408411ff03b9742aac8c97b967541c4de5
trans <transfire@gmail.com>
2008-11-26 10:26:40 -0500

    added statistics.rb

---
+++ lib/stick/trigonometry.rb
@@ -0,0 +1,19 @@
+module Stick
+
+  # TODO: Move trig methods to here.
+  #
+  module Trigonometry
+
+    module Math
+
+    end
+
+  end
+
+end
+
+module Math
+  extend  Trigonometry
+  include Trigonometry
+end
+
