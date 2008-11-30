e41532e17a47209a8759c65588a55620ec6dc29a
trans <transfire@gmail.com>
2008-11-30 11:43:00 -0500

    imporovements to trigonometry.rb

--- lib/stick/trigonometry.rb
+++ lib/stick/trigonometry.rb
@@ -6,24 +6,75 @@ module Stick
     #
     module Math
     
-      # Same as <code>Math::PI</code> - Ludolph's number, <code>PI = 3.141_59..</code>.
+      # Same as <code>Math::PI</code>
+      # Ludolph's number, <code>PI = 3.141_59..</code>.
       PI = ::Math::PI
 
-      PI_by_2 = 0.5 * PI
+      PI_BY_2 = 0.5 * PI
+
+      # Sine of x.
+      def sin(x)
+        ::Math.sin(x)
+      end
+
+      # Hyperbolic Sine of +x+.
+      def sinh(x)
+        ::Math.sinh(x)
+      end
+
+      # Sinc function of +x+.
+      def sinc(x)
+        (x == 0.0) ? 1.0 : sin(x) / x
+      end
+
+      # Tagent of +x+.
+      def tan(x)
+        ::Math.tan(x)
+      end
+
+      # Hyperbolic tagent of +x+.
+      def tanh(x)
+        ::Math.tanh(x)
+      end
+
+      # Arcus tangens of +x+.
+      def atan(x)
+        ::Math.atan(x)
+      end
+
+      # Arcus tangens of +x+ over +y+.
+      def atan2(x, y)
+        ::Math.atan2(x, y)
+      end
+
+      # Same as <code>Math.atanh(+x+)</code> - area tangens hyperbolicus of +x+
+      def atanh(x)
+        ::Math.atanh(x)
+      end
+
+      # Same as <code>Math.cos(+x+)</code> - cosinus of +x+
+      def cos(x)
+        ::Math.cos(x)
+      end
+
+      # Same as <code>Math.cosh(+x+)</code> - cosinus hyperbolicus of +x+
+      def cosh(x)
+        ::Math.cosh(x)
+      end
 
       # Same as <code>Math.acos(+x+)</code> - arcus cotangens of +x+
       def acos(x)
-        Math.acos(x)
+        ::Math.acos(x)
       end
 
       # Same as <code>Math.acosh(+x+)</code> - area cosinus hyperbolicus of +x+
       def acosh(x)
-        Math.acosh(x)
+        ::Math.acosh(x)
       end
 
       # Arcus cotangens of +x+
       def acot(x)
-        PI_by_2 - ::Math.atan(x)
+        PI_BY_2 - atan(x)
       end
 
       # Area cotangens hyperbolicus of +x+
@@ -33,113 +84,62 @@ module Stick
 
       # Arcus cosecans of +x+
       def acsc(x)
-        Math.asin(1.0 / x)
+        asin(1.0 / x)
       end
 
       # Area cosecans hyperbolicus of +x+
       def acsch(x)
-        Math.log(1.0 / x + Math.sqrt(1.0 + 1.0 / (x * x)))
+        ::Math.log(1.0 / x + Math.sqrt(1.0 + 1.0 / (x * x)))
       end
 
       # Arcus secans of +x+
       def asec(x)
-        Math.acos(1.0 / x)
+        acos(1.0 / x)
       end
 
       # Area secans hyperbolicus of +x+
       def asech(x)
-        Math.log((1.0 + ::Math.sqrt(1.0 - x * x)) / x)
+        ::Math.log((1.0 + ::Math.sqrt(1.0 - x * x)) / x)
       end
 
       # Same as <code>Math.asin(+x+)</code> - arcus sinus of +x+
       def asin(x)
-        Math.asin(x)
+        ::Math.asin(x)
       end
 
       # Same as <code>Math.sinh(+x+)</code> - area sinus hyperbolicus of +x+
       def asinh(x)
-        Math.asinh(x)
-      end
-
-      # Arcus tangens of +x+.
-      def atan(x)
-        Math.atan(x)
-      end
-
-      # Arcus tangens of +x+ over +y+.
-      def atan2(x, y)
-        Math.atan2(x, y)
-      end
-
-      # Same as <code>Math.atanh(+x+)</code> - area tangens hyperbolicus of +x+
-      def atanh(x)
-        Math.atanh(x)
-      end
-
-      # Same as <code>Math.cos(+x+)</code> - cosinus of +x+
-      def cos(x)
-        Math.cos(x)
-      end
-
-      # Same as <code>Math.cosh(+x+)</code> - cosinus hyperbolicus of +x+
-      def cosh(x)
-        Math.cosh(x)
+        ::Math.asinh(x)
       end
 
       # Cotangens of +x+
       def cot(x)
-        Math.tan(PI_by_2 - x)
+        tan(PI_BY_2 - x)
       end
 
       # Cotangens hyperbolicus of +x+
       def coth(x)
-        1.0 / ::Math.tanh(x)
+        1.0 / tanh(x)
       end
 
       # Cosecans of +x+
       def csc(x)
-        1.0 / ::Math.sin(x)
+        1.0 / sin(x)
       end
 
       # Cosecans hyperbolicus of +x+
       def csch(x)
-        1.0 / Math.sinh(x)
+        1.0 / sinh(x)
       end
 
       # Secans of +x+.
       def sec(x)
-        1.0 / ::Math.cos(x)
+        1.0 / cos(x)
       end
 
       # Secans hyperbolicus of +x+
       def sech(x)
-        1.0 / ::Math.cosh(x)
-      end
-
-      # Sin of x.
-      def sin(x)
-        ::Math.sin(x)
-      end
-
-      # Sinc function of +x+.
-      #
-      def sinc(x)
-        (x == 0.0) ? 1.0 : ::Math.sin(x) / x
-      end
-
-      # Sin hyperbolicus of +x+.
-      def sinh(x)
-        ::Math.sinh(x)
-      end
-
-      #
-      def tan(x)
-        ::Math.tan(x)
-      end
-
-      #
-      def tanh(x)
-        ::Math.tanh(x)
+        1.0 / cosh(x)
       end
 
     end #module Math
