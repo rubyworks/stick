87837a073916b2438da3b40d2d33843912f3cd9c
7rans <transfire@gmail.com>
2009-12-19 16:40:54 -0500

    added QED and Lemon tests

---
+++ test/case/measure/factor.rb
@@ -0,0 +1,55 @@
+require 'stick2'
+
+TestCase Stick::Measure do
+
+  #include Stick::Units  #TODO: not working, why?
+
+  Unit :factor? => "decerns factors of equal powers" do
+    n1 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new(1))
+    n2 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new(1))
+    n2.assert.factor?(n1)
+    n1 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new, Stick::Units::SI::Meter.new, Stick::Units::SI::Second.new(-1))
+    n2 = Stick::Measure.new(1, Stick::Units::SI::Newton.new)
+    n2.assert.factor?(n1)
+  end
+
+  Unit :factor? => "decerns factors of greater positive powers" do
+    n1 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new(2))
+    n2 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new(1))
+    n2.assert.factor?(n1)
+    n1 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new, Stick::Units::SI::Meter.new(2), Stick::Units::SI::Second.new(-1))
+    n2 = Stick::Measure.new(1, Stick::Units::SI::Newton.new)
+    n2.assert.factor?(n1)
+  end
+
+  Unit :factor? => "decerns factors of lesser negative powers" do
+    n1 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new(-2))
+    n2 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new(-1))
+    n2.assert.factor?(n1)
+    n1 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new, Stick::Units::SI::Meter.new, Stick::Units::SI::Second.new(-2))
+    n2 = Stick::Measure.new(1, Stick::Units::SI::Newton.new)
+    n2.assert.factor?(n1)
+  end
+
+  Unit :factor? => "decerns non-factors" do
+    n1 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new)
+    n2 = Stick::Measure.new(1, Stick::Units::SI::Newton.new)
+    n2.refute.factor?(n1)
+  end
+
+  Unit :factor? => "decerns non-factors of lesser positive powers" do
+    n1 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new(1))
+    n2 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new(2))
+    n2.refute.factor?(n1)
+    n1 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new, Stick::Units::SI::Meter.new(-1), Stick::Units::SI::Second.new(-1))
+    n2 = Stick::Measure.new(1, Stick::Units::SI::Newton.new)
+    n2.refute.factor?(n1)
+  end
+
+  Unit :factor? => "decerns non-factors of greater negative powers" do
+    n1 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new(-1))
+    n2 = Stick::Measure.new(1, Stick::Units::SI::Kilogram.new(-2))
+    n2.refute.factor?(n1)
+  end
+
+end
